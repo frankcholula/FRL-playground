d6819267179668db19514987a76ed0c238118480
frankcholula <lu.phrank@gmail.com>
2025-06-16 16:29:25 +0800

    trying out cartpole.

---
+++ code/cartpole.py
@@ -0,0 +1,14 @@
+import gymnasium as gym
+
+
+env = gym.make("CartPole-v1", render_mode="human")
+observation, info = env.reset()
+
+for t in range(1000):
+    env.render()
+    action = env.action_space.sample()
+    observation, reward, terminated, truncated, info = env.step(action)
+    
+    if terminated or truncated:
+        observation, info = env.reset()
+env.close()
