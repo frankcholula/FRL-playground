63f1f2d7f0632c19b168c82cc701b7f6014221df
frankcholula <lu.phrank@gmail.com>
2025-06-20 15:40:19 +0800

    got ppo working.

--- code/lunarlander.py
+++ code/lunarlander.py
@@ -1,28 +1,37 @@
 import gymnasium as gym
 
-from huggingface_sb3 import load_from_hub, package_to_hub
-from huggingface_hub import (
-    notebook_login,
-)  # To log to our Hugging Face account to be able to upload models to the Hub.
-
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.monitor import Monitor
 
 from wandb.integration.sb3 import WandbCallback
+import os
 
-env = gym.make("LunarLander-v3", continuous=False, render_mode="human")
+env_name = "LunarLander-v3"
+model_name = f"ppo-{env_name}"
 
-observation, info = env.reset()
+if model_name not in os.listdir("models"):
+    env = gym.make(env_name, continuous=False)
+    vec_env = make_vec_env(env, n_envs=16)
+    model = PPO(
+        policy="MlpPolicy",
+        env=vec_env,
+        n_steps=1024,
+        batch_size=64,
+        n_epochs=4,
+        gamma=0.999,
+        gae_lambda=0.98,
+        ent_coef=0.01,
+        verbose=1,
+    )
+    model.learn(total_timesteps=int(10e5))
+    model.save(os.path.join("models", model_name))
 
-for _ in range(200):
-    action = env.action_space.sample()
-    print("Action taken:", action)
-
-    observation, reward, terminated, truncated, info = env.step(action)
-    if terminated or truncated:
-        print("Episode finished after {} timesteps".format(_ + 1))
-        observation, info = env.reset()
-
-env.close()
+else:
+    eval_env = Monitor(gym.make("LunarLander-v3", render_mode="human"))
+    model = PPO.load(model_name, env=eval_env)
+    mean_reward, std_reward = evaluate_policy(
+        model, eval_env, n_eval_episodes=10, deterministic=True
+    )
+    print(f"Mean reward: {mean_reward} +/- {std_reward}")
