279d3ce17238ed7d3421398938afcf765de028c4
frankcholula <lu.phrank@gmail.com>
2025-07-10 22:48:07 +0800

    start flow matching bc.

--- code/pipelines/fm_bc.py
+++ code/pipelines/fm_bc.py
@@ -1,12 +1,26 @@
 from flow_matching import nn as fmnn
 
+import torch
+from torch import nn, Tensor
+
+from flow_matching.path.scheduler import CondOTScheduler
+from flow_matching.path import AffineProbPath
+from flow_matching.solver import Solver, ODESolver
+from flow_matching.utils import ModelWrapper
+
 
 # pi(a) -> a
 # (t, state, action) -> a
 
 
+class Swish(nn.Module):
+    def __init__(self):
+        super().__init__()
 
+    def forward(self, x: Tensor) -> Tensor:
+        return torch.sigmoid(x) * x
+    
 class ConditionalVectorField(nn.Module):
-
+    def __init__(self, )
 
 
